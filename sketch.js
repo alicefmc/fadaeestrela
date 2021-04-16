3f86710caf6441eb6ffcba6d5b1dbe49e869e842
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-04-16 14:09:07 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
-var imgEstrela,imgFt;
-var estrela, corpoEstrela;
+var starImg,bgImg;
+var star, starBody;
 //criar variável para sprite de fada e imgFada
 
-const Mecanismo = Matter.Mecanismo;
-const Mundo = Matter.Mundo;
-const Corpos = Matter.Corpos;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 function preload()
 {
-    imgEstrela = loadImage("images/star.png");
-    imgFt = loadImage("images/starNight.png");
+    starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
     //carregar animação de fada 
 }
 
@@ -21,16 +21,16 @@ function setup() {
 
     //criar sprite de fada e adicionar animação para fada
 
-    estrela = createSprite(650,30);
-    estrela.addImage(imgEstrela);
-    estrela.scale = 0.2;
+    star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
 
-    Mecanismo = Mecanismo.create();
-    Mundo = Mecanismo.Mundo;
+    engine = Engine.create();
+	world = engine.world;
 
-    corpoEstrela = Corpos.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
-    Mundo.add(Mundo, corpoEstrela);
-    
-    Mecanismo.run(Mecanismo);
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
 
 }
