cca723e0eb548aef10d208b196bd1692cbbf2371
alicefmc <alicecosta101208@gmail.com>
2022-12-17 11:17:01 -0300

    PROJETO 22

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var starImg,bgImg;
 var star, starBody;
 //criar variável para sprite de fada e imgFada
+var fada, fadaImg;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -12,6 +13,7 @@ function preload()
     starImg = loadImage("images/star.png");
 	bgImg = loadImage("images/starNight.png");
     //carregar animação de fada 
+    fadaImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png")
 }
 
 function setup() {
@@ -20,6 +22,9 @@ function setup() {
     //escrever código para tocar o som vozFada
 
     //criar sprite de fada e adicionar animação para fada
+    fada = createSprite(150,500)
+    fada.addAnimation("voando",fadaImg)
+    fada.scale = 0.15
 
     star = createSprite(650,30);
 	star.addImage(starImg);
@@ -34,3 +39,27 @@ function setup() {
 	Engine.run(engine);
 
 }
+
+function draw(){
+
+    background(bgImg);
+
+    if (star.y > 470 && starBody.position.y > 470) {
+        Matter.Body.setStatic(starBody,true);
+    }
+    
+    if (keyCode == RIGHT_ARROW) {
+        fada.velocityX = 4
+        star.velocityY = 4
+    }
+
+
+    if(fada.isTouching(star)){
+        star.velocityY = 0;
+        fada.velocityX = 0;
+    }
+    drawSprites();
+
+}
+
+
